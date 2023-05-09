9a9d24bd4a14fa54e832398264171e0e0c6c5fdf
Valentyn Pakhniuk <valentin.pakhniuk@gmail.com>
2023-05-09 22:57:00 +0300

    update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,21 @@
+import { useDispatch, useSelector } from 'react-redux';
 import { ContactForm } from './ContactForm/ContactForm';
 import { ContactList } from './ContactList/ContactList';
 import { Container } from './Container.styled';
 import { Filter } from './Filter/Filter';
+import { useEffect } from 'react';
+import { fetchContactsThunk } from 'redux/operations';
+import { Toaster } from 'react-hot-toast';
+import { selectError, selectIsLoading } from 'redux/selectors';
 
 export const App = () => {
+  const dispatch = useDispatch();
+  const isLoading = useSelector(selectIsLoading);
+  const error = useSelector(selectError);
+
+  useEffect(() => {
+    dispatch(fetchContactsThunk());
+  }, [dispatch]);
   return (
     <Container>
       <h1>Phonebook</h1>
@@ -12,6 +24,8 @@ export const App = () => {
       <h2>Contacts</h2>
       <Filter />
       <ContactList />
+      {isLoading && !error && <b>Request in progress...</b>}
+      <Toaster position="top-right" toastOptions={{ duration: 3000 }} />
     </Container>
   );
 };
