215ddbe5cdc977ca5e108a2b85c52ec0a8a8566d
Valentyn Pakhniuk <valentin.pakhniuk@gmail.com>
2023-05-09 22:57:49 +0300

    update ContactList.jsx

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,22 +1,13 @@
-// import PropTypes from 'prop-types';
 import { useSelector } from 'react-redux';
-import { getContact, getFilterSearchValue } from 'redux/selectors';
+import { selectVisibleContact } from 'redux/selectors';
 import { ContactItem } from 'components/ContactItem/ContactItem';
 
-const getVisibleTasks = (contacts, filterSearchValue) => {
-  return filterSearchValue
-    ? contacts.filter(contact =>
-        contact.name.toLowerCase().includes(filterSearchValue.toLowerCase())
-      )
-    : contacts;
-};
 export const ContactList = () => {
-  const contacts = useSelector(getContact);
-  const filterSearchValue = useSelector(getFilterSearchValue);
-  const visibleContacts = getVisibleTasks(contacts, filterSearchValue);
+  const contacts = useSelector(selectVisibleContact);
+
   return (
     <ul>
-      {visibleContacts.map(contact => {
+      {contacts.map(contact => {
         return <ContactItem contact={contact} key={contact.id} />;
       })}
     </ul>
