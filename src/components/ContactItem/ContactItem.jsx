20fc0a9c157821e8c9acb9de187a5c6c19c4d983
Valentyn Pakhniuk <valentin.pakhniuk@gmail.com>
2023-05-09 22:57:37 +0300

    update ContactItem.jsx

--- src/components/ContactItem/ContactItem.jsx
+++ src/components/ContactItem/ContactItem.jsx
@@ -1,29 +1,32 @@
 import { Button } from 'components/Button/Button.styled';
 import { useDispatch } from 'react-redux';
-import { deleteContact } from 'redux/contactsSlice';
 import PropTypes from 'prop-types';
 import { Item } from './ContactItem.styled';
+import { deleteContactsThunk } from 'redux/operations';
 
 export const ContactItem = ({ contact }) => {
   const dispatch = useDispatch();
-
-  const handleDelete = () => dispatch(deleteContact(contact.id));
+  const handleDelete = () => dispatch(deleteContactsThunk(contact.id));
   return (
-    <Item key={contact.id}>
-      <div>
-        <p>
-          {contact.name}: {contact.number}
-        </p>
-        <Button type="button" onClick={() => handleDelete(contact.id)}>
-          Delete
-        </Button>
-      </div>
-    </Item>
+      <Item key={contact.id}>
+        <div>
+          <p>
+            {contact.name}: {contact.number}
+          </p>
+          <Button
+            type="button"
+            onClick={() => handleDelete(contact.id)}
+          >
+            Delete
+          </Button>
+        </div>
+      </Item>
   );
 };
 
 ContactItem.propTypes = {
   contact: PropTypes.exact({
+    createdAt: PropTypes.string.isRequired,
     id: PropTypes.string.isRequired,
     name: PropTypes.string.isRequired,
     number: PropTypes.string.isRequired,
